507a7f5d9592112030c4a9d76d4fcceb3c4e51cd
Andrew Johnson <andrewjohnson8819@gmail.com>
2015-12-04 16:47:15 -0500

    added troll-quotes controller, deleted authors, images, quotes/updated schema -AJ

--- db/schema.js
+++ db/schema.js
@@ -1,7 +1,26 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://locahost/troll_quotes");
 
+var Schema = mongoose.Schema;
+var ObjectId = Schema.Types.objectId;
 
+var AuthorSchema = new Schema(
+  {
+    name: String
+  }
+);
+
+var QuoteSchema = new Schema(
+  {
+    text: String
+  }
+);
+
+var ImageSchema = new Schema(
+  {
+    url: String
+  }
+);
 
 
 
