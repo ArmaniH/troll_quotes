ebc740bb2c44a13180bf26b073ceea8c42a910e6
Andrew Johnson <andrewjohnson8819@gmail.com>
2015-12-04 15:52:07 -0500

    added schema, seeds, indexView files -AJ

---
+++ db/schema.js
@@ -0,0 +1,29 @@
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://locahost/troll_quotes");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// {
+//   science-fiction: [
+//       "I am your father",
+//       "Use the force, Luke",
+//       "There is no try"
+//       ]
+//   ],
+//   politics: [
+//       "I am not a crook",
+//       "I did not have sex with that woman",
+//       "But what you can do for your country"
+//   ]
+// }
