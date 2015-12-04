57a23d819be8bc79b681c6eed874753d3a4aebc5
Britton Walker <bwalker1801@gmail.com>
2015-12-04 14:08:57 -0500

    update image spec file BW

--- spec/image-spec.js
+++ spec/image-spec.js
@@ -1,6 +1,6 @@
 var Image = require('../models/image.js');
 
-describe( "Troll Image", function(){
+describe( "An Image", function(){
   it("should have a url", function(){
     var image = new Image("http://imgur.com/Q84N3b6");
     expect(image.url).toBeDefined(jasmine.any(String));
