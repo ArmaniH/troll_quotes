4ca53add3c769c6c12bca1a6d5e7930c3922c815
Britton Walker <bwalker1801@gmail.com>
2015-12-07 14:41:28 -0500

    change image links, add json routes, make index.html view with css -BW

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+var bodyParser = require("body-parser");
+
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(bodyParser.json());
+
+// app.get("/", function(req, res){
+//   res.render("layout.hbs");
+// });
+
+
+app.use("/", require("./controllers/troll-quotes"));
+
+app.listen(3000, function(){
+  console.log("Listening on port 3000");
+});
---
+++ public/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  Author.fetch().then(function(authors){
+    for(var i = 0; i <)
+    var p = document.createElement('p');
+    p.text()
+    document.body.appendChild(p);
+
+});
