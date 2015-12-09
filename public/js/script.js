11719b7969c17dc3669b62a6267dc6e64ac84c9d
Britton Walker <bwalker1801@gmail.com>
2015-12-09 16:01:36 -0500

    fix on load generation -BW

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
-
+  var randomTroll = "http://localhost:3000/randomTroll";
+  $.getJSON(randomTroll)
+  .done(function(res){
+    console.log(res);
+    var author = res.author.name;
+    var quote = res.quote.text;
+    var image = res.image.url;
+    var authorTag = $('.author');
+    var quoteTag = $('.quote');
+    var imgTag = $('.image');
+    imgTag.attr('src', image);
+    authorTag.text("- " + author);
+    quoteTag.text('"' + quote + '"');
+  })
   $('.animated-button').click(function(e){
     e.preventDefault();
+    // $('.image').toggle('display')
     var randomTroll = "http://localhost:3000/randomTroll";
     $.getJSON(randomTroll)
     .done(function(res){
@@ -16,5 +30,14 @@ $(document).ready(function(){
       authorTag.text("- " + author);
       quoteTag.text('"' + quote + '"');
     })
-  })
+
+  });
+
+  $('.button').click(function(e){
+    e.preventDefault();
+    var authorTag = $('.author');
+    console.log(authorTag.text());
+  });
+
+
 });
