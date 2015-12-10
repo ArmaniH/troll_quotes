d5fcb5688480cca0aa7e7df230dbde038d2d3cdc
Britton Walker <bwalker1801@gmail.com>
2015-12-10 14:51:32 -0500

    fix unclosed styles in css add hamburger

--- public/js/script.js
+++ public/js/script.js
@@ -66,6 +66,23 @@ $(document).ready(function() {
     console.log(response);
   });
 
+  $('.menu').on('click', function(e) {
+  e.preventDefault();
+  $(this).next().toggleClass('active');
+});
+
+
+
+// Courtesy: http://stackoverflow.com/questions/1403615/use-jquery-to-hide-a-div-when-the-user-clicks-outside-of-it
+$(document).click(function (e) {
+    var containers = $('.menu, .nav');
+    if (!containers.is(e.target) && containers.has(e.target).length === 0){
+        containers.removeClass('active');
+    }
+});
+
+
+
 
 
 });
