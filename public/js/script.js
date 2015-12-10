c43d190164fb014da5fdbb9f8296adf9046a5179
Britton Walker <bwalker1801@gmail.com>
2015-12-10 11:32:01 -0500

    deleted extra comma on package.json and fixed a button in script

--- public/js/script.js
+++ public/js/script.js
@@ -1,24 +1,8 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  var response;
   var randomTroll = "http://localhost:3000/randomTroll";
   $.getJSON(randomTroll)
-  .done(function(res){
-    console.log(res);
-    var author = res.author.name;
-    var quote = res.quote.text;
-    var image = res.image.url;
-    var authorTag = $('.author');
-    var quoteTag = $('.quote');
-    var imgTag = $('.image');
-    imgTag.attr('src', image);
-    authorTag.text("- " + author);
-    quoteTag.text('"' + quote + '"');
-  })
-  $('.animated-button').click(function(e){
-    e.preventDefault();
-    // $('.image').toggle('display')
-    var randomTroll = "http://localhost:3000/randomTroll";
-    $.getJSON(randomTroll)
-    .done(function(res){
+    .done(function(res) {
       console.log(res);
       var author = res.author.name;
       var quote = res.quote.text;
@@ -30,13 +14,31 @@ $(document).ready(function(){
       authorTag.text("- " + author);
       quoteTag.text('"' + quote + '"');
     })
+  $('.animated-button').click(function(e) {
+    e.preventDefault();
+    // $('.image').toggle('display')
+    var randomTroll = "http://localhost:3000/randomTroll";
+    $.getJSON(randomTroll)
+      .done(function(res) {
+        response = res;
+        console.log(res);
+        var author = res.author.name;
+        var quote = res.quote.text;
+        var image = res.image.url;
+        var authorTag = $('.author');
+        var quoteTag = $('.quote');
+        var imgTag = $('.image');
+        imgTag.attr('src', image);
+        authorTag.text("- " + author);
+        quoteTag.text('"' + quote + '"');
+      })
 
-  })
+  });
 
-var i = 1;
+  var i = 1;
   $('#export').on('click', function() {
     html2canvas($('#rectangle'), {
-      "logging" : false,
+      "logging": false,
       "allowTaint": true,
       onrendered: function(canvas) {
         // document.body.appendChild(canvas);
@@ -44,20 +46,26 @@ var i = 1;
         $("#favorites").append(canvas);
         // return Canvas2Image.saveAsPNG(canvas);
         // document.body.removeChild(canvas);
-         var d= ($('<input/>',{ type:"button", id:"delete"+i, value:"delete"}));
-         $("#favorites").append(d);
-         i++;
+        var d = ($('<input/>', {
+          type: "button",
+          id: "delete" + i,
+          value: "delete"
+        }));
+        $("#favorites").append(d);
+        i++;
       },
     });
   });
 
-   $('#delete').on('click', function() {
-        document.body.remove();
-    });
+  $('#delete').on('click', function() {
+    document.body.remove();
+  });
+
+  $('#button').click(function(e) {
+    e.preventDefault();
+    console.log(response);
 });
 
-  $('.button').click(function(e){
-    e.preventDefault();
-    var authorTag = $('.author');
-    console.log(authorTag.text());
-  });
+
+
+});
